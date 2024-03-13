dc954a4420aa694e11d8b32d832a01d499d251d5
Frans Johansson <fransjohansson98@gmail.com>
2024-03-13 22:04:49 +0100

    Implement a class for managing pairs of pipes

--- pyflap.py
+++ pyflap.py
@@ -27,6 +27,36 @@ BIRD_IMAGES = [
 ]
 
 
+class Pipes:
+    def __init__(self, upper: pygame.Rect, lower: pygame.Rect) -> None:
+        self.upper = upper
+        self.lower = lower
+
+    def update(self, dt: int) -> None:
+        self.upper.move_ip(-PIPE_SCROLL_VELOCITY * dt, 0)
+        self.lower.move_ip(-PIPE_SCROLL_VELOCITY * dt, 0)
+
+    def render(self, screen: pygame.SurfaceType):
+        pygame.draw.rect(surface=screen, color="darkgreen", rect=self.upper)
+        pygame.draw.rect(surface=screen, color="darkgreen", rect=self.lower)
+
+    def check_collision(self, other: pygame.Rect) -> bool:
+        return self.upper.colliderect(other) or self.lower.colliderect(other)
+
+    @classmethod
+    def spawn(cls) -> "Pipes":
+        height_lower = random.uniform(0, (HEIGHT - PIPE_GAP) / HEIGHT)
+        lower = pygame.Rect(0, 0, PIPE_WIDTH, HEIGHT * height_lower)
+        lower.midbottom = (WIDTH, HEIGHT)
+        upper = pygame.Rect(0, 0, PIPE_WIDTH, HEIGHT - lower.height - PIPE_GAP)
+        upper.midtop = (WIDTH, 0)
+        return cls(upper, lower)
+
+    @staticmethod
+    def despawn(pipes: list["Pipes"]) -> list["Pipes"]:
+        return [pipe for pipe in pipes if pipe.lower.right > 0]
+
+
 class State:
     def __init__(self):
         self.reset()
@@ -35,7 +65,7 @@ class State:
     def reset(self):
         self.bird = pygame.Rect(0, 0, BIRD_SIZE, BIRD_SIZE)
         self.bird.center = (WIDTH // 3, HEIGHT // 2)
-        self.pipes = spawn_pipe()
+        self.pipes = [Pipes.spawn()]
         self.bird_velocity = 0.0
         self.pipe_spawn_countup = 0.0
         self.score = 0
@@ -67,17 +97,17 @@ class State:
             self.bird_velocity = 0
 
         for pipe in self.pipes:
-            pipe.move_ip(-PIPE_SCROLL_VELOCITY * dt, 0)
+            pipe.update(dt)
 
         self.pipe_spawn_countup += (dt / 1000) * PIPE_SPAWN_FREQUENCY
         if self.pipe_spawn_countup > 1:
-            self.pipes.extend(spawn_pipe())
+            self.pipes.append(Pipes.spawn())
             self.score += 1
             self.pipe_spawn_countup = 0
 
-        self.pipes = despawn_pipes(self.pipes)
+        self.pipes = Pipes.despawn(self.pipes)
 
-        if bird_has_crashed(self.pipes, self.bird):
+        if any(pipe.check_collision(self.bird) for pipe in self.pipes):
             self.best_score = max(self.best_score, self.score)
             self.reset()
 
@@ -103,7 +133,7 @@ class Game:
         self.screen.blit(BIRD_IMAGES[self.state.bird_frame], self.state.bird)
 
         for pipe in self.state.pipes:
-            pygame.draw.rect(surface=self.screen, color="darkgreen", rect=pipe)
+            pipe.render(self.screen)
 
         score = self.font.render(f"Score: {self.state.score}", True, "black")
         best_score = self.font.render(
